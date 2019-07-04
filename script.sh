779d2d007bfb8ba630924fb038e843ae2f0ed72b
Miguel Saitz <mie.saitz@gmail.com>
2019-07-04 23:29:29 +0100

    stick to naming convention + style

--- script.sh
+++ script.sh
@@ -1,65 +1,69 @@
 #!/bin/bash
 
-trap closeFile INT
+ALLOW_DELETION=$1
+RECREATED_KEYS=0
+FILE="recreated_keys_$(date +%H%M%S).json"
 
-closeFile() {
-  if [ $recreatedKeys -ne 0 ]; then
-    echo "]}" >> $file
-    echo "Re-created keys exported to $file"
+trap close_file INT
+
+close_file() {
+  if [ $RECREATED_KEYS -ne 0 ]; then
+    echo "]}" >> $FILE
+    echo "Re-created keys have been exported to $FILE"
   fi
   exit 0
 }
 
-getJsonElement() {
+get_json_element() {
   echo $2 | jq '.'[$1] | jq -r $3
 }
 
-accountInteractor() {
+account_interactor() {
   while true; do
     echo "-- Select an action to perform for key --"
     printf "    (s) Skip" 
     printf "    (d) Delete"
-    read -p "    (r) Recreate"$'\n' choice
+    read -p "    (r) Re-create"$'\n' choice
   
     case $choice in
         [Dd]* ) echo "deleting key..."
-          deleteKey "$1" "$2"; break;; 
+          delete_key "$1" "$2"; break;; 
         [Rr]* ) echo "recreating key..." 
-          recreateKey "$1" "$2"; break;;
+          recreate_key "$1" "$2"; break;;
         [Ss]* ) echo skipped; break;;
         * ) echo "Invalid action"; break;;
     esac
   done
 }
 
-deleteKey() {
+delete_key() {
   aws iam delete-access-key --access-key-id "$1" --user-name "$2"
 }
 
-recreateKey() {
-  if [ $recreatedKeys -eq 0 ]; then
-    createFile
+recreate_key() {
+  if [ $RECREATED_KEYS -eq 0 ]; then
+    create_file
   fi
   
-  deleteKey "$1" "$2"
-  newKey=$(aws iam create-access-key --user-name $2 | jq '.AccessKey')  
-  appendToFile "$newKey"
-  ((recreatedKeys++))
+  delete_key "$1" "$2"
+  new_key=$(aws iam create-access-key --user-name $2 | jq '.AccessKey')  
+  append_to_file "$new_key"
+  ((RECREATED_KEYS++))
 }
 
-createFile() {
-  touch $file
-  echo '{ "AccessKeys": [ ' >> $file 
+create_file() {
+  touch $FILE
+  echo '{ "AccessKeys": [ ' >> $FILE 
 }
 
-appendToFile() {
-  if [ $recreatedKeys -ne 0 ]; then
-    echo -n "," >> $file
+append_to_file() {
+  if [ $RECREATED_KEYS -ne 0 ]; then
+    echo -n "," >> $FILE
   fi
-  echo $1 >> $file
+  echo $1 >> $FILE
 }
 
-checkIfUserHasKeys() {
+check_if_user_has_keys() {
   keys=$(aws iam list-access-keys --user-name "$user" | jq '.AccessKeyMetadata')
   if [ "$keys" = "[]" ]; then
     return 1
@@ -68,38 +72,35 @@ checkIfUserHasKeys() {
   fi
 }
 
-showKeys() {
-  keysLen=$(echo "$1" | jq length) 
-  for (( j=0; j<$keysLen; j++ ))
+show_keys() {
+  keys_len=$(echo "$1" | jq length) 
+  for (( j=0; j<$keys_len; j++ ))
   do 
-    keyId=$(getJsonElement $j "$1" ".AccessKeyId")
-    #created=$(date -jf '%Y-%m-%dT%H:%M:%SZ' $(getJsonElement $j "$1" ".CreateDate"))
-    created=$(date -d $(getJsonElement $j "$1" ".CreateDate") '+%d %b %Y')
-    echo "    Key Id: $keyId ($(getJsonElement $j "$1" ".Status"))"
+    key_id=$(get_json_element $j "$1" ".AccessKeyId")
+    #created=$(date -jf '%Y-%m-%dT%H:%M:%SZ' $(get_json_element $j "$1" ".CreateDate"))
+    created=$(date -d $(get_json_element $j "$1" ".CreateDate") '+%d %b %Y')
+    echo "    Key Id: $key_id ($(get_json_element $j "$1" ".Status"))"
     echo "    Created on $created"
     
-    if [ "$allowDeletion" = "allow-deletion" ]; then 
-      accountInteractor "$keyId" "$user" 
+    if [ "$ALLOW_DELETION" = "allow-deletion" ]; then 
+      account_interactor "$key_id" "$user" 
     fi
-    if (( $keysLen - $j > 1  )); then echo; fi  
+
+    if (( $keys_len - $j > 1  )); then echo; fi  
   done
 }
 
-allowDeletion=$1
-recreatedKeys=0
-file="recreated_keys$(date +%H%m%S).json"
-
 users=$(aws iam list-users | jq '.Users')
-usersLen=$(echo "$users" | jq length)
+users_len=$(echo "$users" | jq length)
 
-for (( i=0; i<$usersLen; i++ ))
+for (( i=0; i<$users_len; i++ ))
 do
-  user=$(getJsonElement $i "$users" ".UserName") 
-  echo "[$(($i + 1))/$usersLen] IAM user: $user"
+  user=$(get_json_element $i "$users" ".UserName") 
+  echo "[$(($i + 1))/$users_len] IAM user: $user"
   
-  checkIfUserHasKeys
+  check_if_user_has_keys
   if [ $? -eq 0 ]; then
-    showKeys "$keys"
+    show_keys "$keys"
   else
     echo "    User has no generated keys"
   fi
@@ -108,4 +109,4 @@ do
   echo
 done
 
-closeFile
+close_file
